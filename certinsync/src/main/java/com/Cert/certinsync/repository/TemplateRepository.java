2b53d8c3c722da9bf10e23b6b7a6a771cae76397
Nimisha negi <nimishanegi2003@gmail.com>
2025-07-20 18:39:21 +0530

    correct files

--- certinsync/src/main/java/com/Cert/certinsync/repository/TemplateRepository.java
+++
@@ -1,7 +0,0 @@
-package com.Cert.certinsync.repository;
-
-import com.Cert.certinsync.entity.TemplateEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TemplateRepository extends JpaRepository<TemplateEntity, Long> {
-}
--- certinsync/src/main/java/com/Cert/certinsync/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.Cert.certinsync.repository;
-import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.Cert.certinsync.entity.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByEmail(String email); // THIS must return Optional<User>
-}
-
